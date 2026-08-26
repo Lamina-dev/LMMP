a2cbf0e3c18aea3b466617bc43fc9b1293b36af0
Jericho Knox <hjimmyk@163.com>
2026-08-26 23:29:38 +0800

    tune: update tune programa and params

--- tune/lmmp/src/tune_factors_mul_n.c
+++ tune/lmmp/src/tune_factors_mul_n.c
@@ -29,6 +29,7 @@ static void set_threshold(uint64_t v) { lmmp_tune_FACTORS_MUL_N_THRESHOLD = v; }
 
 typedef struct {
     mp_ptr dst;
+    mp_size_t rn;
     fac_t* fac;
     uint nfactors;
 } factors_ctx;
@@ -47,21 +48,25 @@ static uint next_prime(uint v) {
 
 static void factors_ctx_init(factors_ctx* c, uint nfactors) {
     c->nfactors = nfactors;
-    c->dst = (mp_ptr)lmmp_alloc((size_t)(2 * nfactors + 4) * sizeof(mp_limb_t));
+    // 按每个质数的幂次为2个两个limb来计算得到的长度
+    c->rn = 2 * nfactors + 4;
+    c->dst = (mp_ptr)lmmp_alloc((size_t)(c->rn) * sizeof(mp_limb_t));
     c->fac = (fac_t*)lmmp_alloc((size_t)nfactors * sizeof(fac_t));
+    c->fac[0].j = 1;
+    c->fac[0].f = 1;
     uint p = 2;
-    for (uint i = 0; i < nfactors; ++i) {
-        if (i != 0)
-            p = next_prime(p);
+    for (uint i = 1; i < nfactors; ++i) {
+        p = next_prime(p);
         c->fac[i].f = p;
-        c->fac[i].j = (i % 11 == 0) ? 2u : 1u;
+        c->fac[i].j = nfactors / (p - 1);
+        if (c->fac[i].j == 0)
+            c->fac[i].j = 1;
     }
 }
 
 static double bench_factors(void* v) {
     factors_ctx* c = (factors_ctx*)v;
-    (void)lmmp_factors_mul_(c->dst, 2 * (mp_size_t)c->nfactors + 4,
-                            c->fac, c->nfactors);
+    (void)lmmp_factors_mul_(c->dst, c->rn, c->fac, c->nfactors);
     return 0.0;
 }
 
@@ -94,7 +99,7 @@ int tune_run_factors_mul_n(void) {
     spec.low_name = "factors_sequential";
     spec.high_name = "factors_divide";
     spec.lo = 4;
-    spec.hi = 128;
+    spec.hi = 64;
     spec.pred = TUNE_HIGH_WHEN_GT;
     spec.get = get_threshold;
     spec.set = set_threshold;
