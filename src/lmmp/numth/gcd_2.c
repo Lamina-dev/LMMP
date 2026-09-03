8b10377907df68e4a4c4930da081924bc498f127
Jericho Knox <hjimmyk@163.com>
2026-09-03 14:44:20 +0800

    perf: refactor gcd_11 and gcd_22

--- src/lmmp/numth/gcd_2.c
+++ src/lmmp/numth/gcd_2.c
@@ -1,11 +1,11 @@
-﻿/**
+/**
  *  Copyright (C) 2026 HJimmyK(Jericho Knox)
  *
  *  This file is part of LMMP.
  *
  *  LMMP is free software: you can redistribute it and/or modify it under
  *  the terms of the GNU Lesser General Public License (LGPL) as published
- *   by the Free Software Foundation; either version 3 of the License, or
+ *  by the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed WITHOUT ANY WARRANTY.
@@ -13,16 +13,42 @@
  *  See <https://www.gnu.org/licenses/>.
  */
 
-#include "../../../include/lmmp/impl/longlong.h"
+#include "../../../include/lmmp/impl/inlines.h"
 #include "../../../include/lmmp/lmmpn.h"
 #include "../../../include/lmmp/numth.h"
 
 
-
-/*
- FIXME: 内联lmmp_tailing_zeros_()函数，使用编译器内置函数，可以显著提升性能。
- 尝试优化控制流，减少分支与跳转。
+/**
+ * @brief 128 位尾零数
+ * @return if (lo != 0) return ctz(lo); else return 64 + ctz(hi);
  */
+static inline int ctz_u128(mp_limb_t lo, mp_limb_t hi) {
+    return lo ? lmmp_tailing_zeros_(lo) : (64 + lmmp_tailing_zeros_(hi));
+}
+
+static inline void lshr_u128(mp_limb_t* lo, mp_limb_t* hi, int k) {
+    if (k == 0)
+        return;
+    if (k < 64) {
+        *lo = (*lo >> k) | (*hi << (64 - k));
+        *hi >>= k;
+    } else {
+        *lo = *hi >> (k - 64);
+        *hi = 0;
+    }
+}
+
+static inline void lshl_u128(mp_limb_t* lo, mp_limb_t* hi, int k) {
+    if (k == 0)
+        return;
+    if (k < 64) {
+        *hi = (*lo >> (64 - k)) | (*hi << k);
+        *lo <<= k;
+    } else {
+        *hi = *lo << (k - 64);
+        *lo = 0;
+    }
+}
 
 mp_size_t lmmp_gcd_22_(mp_ptr dst, mp_srcptr up, mp_srcptr vp) {
     lmmp_param_assert(dst != NULL);
@@ -30,133 +56,87 @@ mp_size_t lmmp_gcd_22_(mp_ptr dst, mp_srcptr up, mp_srcptr vp) {
     lmmp_param_assert(vp != NULL);
     lmmp_param_assert(!(up[1] == 0 && up[0] == 0));
     lmmp_param_assert(!(vp[1] == 0 && vp[0] == 0));
-    mp_limb_t u[2] = { up[0], up[1] };
-    mp_limb_t v[2] = { vp[0], vp[1] };
-    int k, cnt;
+    mp_limb_t u0 = up[0], u1 = up[1];
+    mp_limb_t v0 = vp[0], v1 = vp[1];
 
-    if (u[1] == 0 && v[1] == 0) {
-        dst[0] = lmmp_gcd_11_(u[0], v[0]);
+    if ((u1 | v1) == 0) {
+        dst[0] = lmmp_gcd_11_(u0, v0);
         dst[1] = 0;
         return 1;
-    } else if (u[1] == 0) {
-        cnt = lmmp_tailing_zeros_(u[0] | v[0]);
-        u[0] = u[0] >> lmmp_tailing_zeros_(u[0]);
-        goto gcd_1_2;
-    } else if (v[1] == 0) {
-        cnt = lmmp_tailing_zeros_(u[0] | v[0]);
-        v[0] = v[0] >> lmmp_tailing_zeros_(v[0]);
-        goto gcd_2_1;
     }
 
-    if (u[0] == 0 && v[0] == 0) {
-        dst[0] = lmmp_gcd_11_(u[1], v[1]); 
-        dst[1] = 0;
-        return 1;
-    } else if (u[0] == 0) {
-        u[0] = u[1] >> lmmp_tailing_zeros_(u[1]);
-        cnt = lmmp_tailing_zeros_(v[0]);
-        goto gcd_1_2;
-    } else if (v[0] == 0) {
-        v[0] = v[1] >> lmmp_tailing_zeros_(v[1]);
-        cnt = lmmp_tailing_zeros_(u[0]);
-        goto gcd_2_1;
-    }
-    cnt = lmmp_tailing_zeros_(u[0] | v[0]);
-    k = lmmp_tailing_zeros_(u[0]);
+    // cnt = min(tz(u), tz(v)) 为 gcd 中的 2 因子幂
+    int ku = ctz_u128(u0, u1);
+    int kv = ctz_u128(v0, v1);
+    int cnt = ku < kv ? ku : kv;
+    lshr_u128(&u0, &u1, ku);
+    lshr_u128(&v0, &v1, kv);
+    // u, v 均为奇数；去冗余最低位（隐式最低位表示），腾出符号位
+    u0 = (u0 >> 1) | (u1 << 63);
+    u1 >>= 1;
+    v0 = (v0 >> 1) | (v1 << 63);
+    v1 >>= 1;
 
-    if (k > 0)
-        _u128lshr(u, u, k);
-    k = lmmp_tailing_zeros_(v[0]);
-    if (k > 0)
-        _u128lshr(v, v, k);
-    while (!(u[0] == v[0] && u[1] == v[1])) {
-        if (u[1] == 0 && v[1] != 0) goto gcd_1_2;
-        if (v[1] == 0 && u[1] != 0) goto gcd_2_1;
-        if (u[1] == 0 && v[1] == 0) goto gcd_1_1;
+    while (u1 | v1) {   // 至少一方仍为双 limb
+        mp_limb_t t0 = u0 - v0;
+        mp_limb_t t1 = u1 - v1 - (u0 < v0);
+        mp_limb_t m = (mp_limb_t)((slong)t1 >> 63);   // u<v -> 1
+        if (t0 == 0) {
+            if (t1 == 0)
+                break;
 
-        if (_u128cmp(u, v)) {
-            _u128sub(v, v, u);
-            if (v[0] == 0) {
-                v[0] = v[1] >> lmmp_tailing_zeros_(v[1]);
-                goto gcd_2_1;
-            } else if (v[1] == 0) {
-                v[0] = v[0] >> lmmp_tailing_zeros_(v[0]);
-                goto gcd_2_1;
-            }
-            k = lmmp_tailing_zeros_(v[0]);
-            // k > 0
-            _u128lshr(v, v, k);
+            v1 += m & t1;                // v1 = min(u1, v1)（v0 == u0，无需更新）
+            u0 = (t1 ^ m) - m;           // |u1 - v1|
+            u0 >>= lmmp_tailing_zeros_(t1) + 1;
+            u1 = 0;
         } else {
-            _u128sub(u, u, v);
-            if (u[0] == 0) {
-                u[0] = u[1] >> lmmp_tailing_zeros_(u[1]);
-                goto gcd_1_2;
-            } else if (u[1] == 0) {
-                u[0] = u[0] >> lmmp_tailing_zeros_(u[0]);
-                goto gcd_1_2;
+            int c = lmmp_tailing_zeros_(t0) + 1;
+            // v = min(u, v)（128 位加法 v += m & t）
+            mp_limb_t w0 = m & t0;
+            mp_limb_t w1 = m & t1;
+            v0 += w0;
+            v1 += w1 + (v0 < w0);
+
+            u0 = (t0 ^ m) - m;
+            u1 = t1 ^ m;
+            if (c == 64) {
+                u0 = u1;
+                u1 = 0;
+            } else {
+                u0 = (u0 >> c) | (u1 << (64 - c));
+                u1 >>= c;
             }
-            k = lmmp_tailing_zeros_(u[0]);
-            // k > 0
-            _u128lshr(u, u, k);
         }
     }
-    dst[0] = u[0];
-    dst[1] = u[1];
+
+    mp_limb_t g0, g1;   // 奇数 gcd
+    if (u1 | v1) {
+        g0 = (u0 << 1) | 1;
+        g1 = (u1 << 1) | (u0 >> 63);
+    } else {
+        while ((u0 | v0) >> 63) {   // 任一 >= 2^63 则真值 >= 2^64，至多两次迭代
+            mp_limb_t t = u0 - v0;
+            if (t == 0)
+                break;   // u == v
+            mp_limb_t m = -(mp_limb_t)(u0 < v0);   // 符号掩码
+            v0 += m & t;                           // v = min(u0, v0)
+            u0 = (t ^ m) - m;                      // |u0 - v0|
+            u0 = (u0 >> 1) >> lmmp_tailing_zeros_(t);
+        }
+        if (u0 == v0) {
+            g0 = (u0 << 1) | 1;
+            g1 = u0 >> 63;
+        } else {
+            g0 = lmmp_gcd_11_((u0 << 1) + 1, (v0 << 1) + 1);
+            g1 = 0;
+        }
+    }
+
+    dst[0] = g0;
+    dst[1] = g1;
     if (cnt > 0)
-        _u128lshl(dst, dst, cnt);
-    return 2;
-
-    gcd_1_2:   // [u,1]  ,  [v,2]
-        k = lmmp_tailing_zeros_(v[0]);
-        if (k > 0)
-            _u128lshr(v, v, k);
-        while (v[1] != 0) {
-            _u128sub64(v, v, u[0]);
-            if (v[1] == 0) 
-                goto gcd_1_1;
-            if (v[0] == 0) {
-                v[0] = v[1] >> lmmp_tailing_zeros_(v[1]);
-                goto gcd_1_1;
-            }
-            k = lmmp_tailing_zeros_(v[0]);
-            // k > 0
-            _u128lshr(v, v, k);
-        }
-        goto gcd_1_1;
-
-    gcd_2_1:   // [u,2]  ,  [v,1]
-        k = lmmp_tailing_zeros_(u[0]);
-        if (k > 0)
-            _u128lshr(u, u, k);
-        while (u[1] != 0) {
-            _u128sub64(u, u, v[0]);
-            if (u[1] == 0)
-                goto gcd_1_1;
-            if (u[0] == 0) {
-                u[0] = u[1] >> lmmp_tailing_zeros_(u[1]);
-                goto gcd_1_1;
-            }
-            k = lmmp_tailing_zeros_(u[0]);
-            // k > 0
-            _u128lshr(u, u, k);
-        }
-        goto gcd_1_1;
-
-    gcd_1_1:   // [u,1]  ,  [v,1]
-        while (u[0] != v[0]) {
-            if (u[0] > v[0]) {
-                u[0] -= v[0];
-                u[0] >>= lmmp_tailing_zeros_(u[0]);
-            } else {
-                v[0] -= u[0];
-                v[0] >>= lmmp_tailing_zeros_(v[0]);
-            }
-        }
-        dst[0] = u[0];
-        dst[1] = 0;
-        if (cnt > 0)
-            _u128lshl(dst, dst, cnt);
-        return dst[1] == 0 ?  1  :  2;
+        lshl_u128(&dst[0], &dst[1], cnt);
+    return dst[1] == 0 ? 1 : 2;
 }
 
 mp_size_t lmmp_gcd_2_(mp_ptr dst, mp_srcptr up, mp_size_t un, mp_srcptr vp) {
