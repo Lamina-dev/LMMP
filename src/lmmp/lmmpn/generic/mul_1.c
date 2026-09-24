ea4e128e576aad9407acd8fd7d02eed420dab0cf
Jericho Knox <hjimmyk@163.com>
2026-09-24 21:21:04 +0800

    perf: improve generic function

--- src/lmmp/lmmpn/generic/mul_1.c
+++ src/lmmp/lmmpn/generic/mul_1.c
@@ -18,9 +18,34 @@
 
 
 mp_limb_t lmmp_mul_1_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_limb_t x) {
+    /*
+        链式 (a_{k-1} >> 64) 进位提取使 GCC/Clang 均生成 add/adc 融合序列，
+        单 limb 链长约 2c，展开宽度 W 可将链摊薄为 (W+1)/W c：
+          - clang 会把 4 路链式源码回卷成 2 路（实测慢约 7~12%），6 路是其
+            能保持的最小宽度；
+          - gcc 恰好相反，4 路最优、6 路慢约 4%。
+        故按编译器选择展开宽度。
+    */
     mp_limb_t cl = 0;
     mp_size_t i = 0;
 
+#if defined(__clang__)
+    for (; i + 6 <= na; i += 6) {
+        __uint128_t a0 = (__uint128_t)numa[i + 0] * x + cl;
+        __uint128_t a1 = (__uint128_t)numa[i + 1] * x + (mp_limb_t)(a0 >> 64);
+        __uint128_t a2 = (__uint128_t)numa[i + 2] * x + (mp_limb_t)(a1 >> 64);
+        __uint128_t a3 = (__uint128_t)numa[i + 3] * x + (mp_limb_t)(a2 >> 64);
+        __uint128_t a4 = (__uint128_t)numa[i + 4] * x + (mp_limb_t)(a3 >> 64);
+        __uint128_t a5 = (__uint128_t)numa[i + 5] * x + (mp_limb_t)(a4 >> 64);
+        dst[i + 0] = (mp_limb_t)a0;
+        dst[i + 1] = (mp_limb_t)a1;
+        dst[i + 2] = (mp_limb_t)a2;
+        dst[i + 3] = (mp_limb_t)a3;
+        dst[i + 4] = (mp_limb_t)a4;
+        dst[i + 5] = (mp_limb_t)a5;
+        cl = (mp_limb_t)(a5 >> 64);
+    }
+#else
     for (; i + 4 <= na; i += 4) {
         __uint128_t a0 = (__uint128_t)numa[i + 0] * x + cl;
         __uint128_t a1 = (__uint128_t)numa[i + 1] * x + (mp_limb_t)(a0 >> 64);
@@ -32,6 +57,7 @@ mp_limb_t lmmp_mul_1_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_limb_t x) {
         dst[i + 3] = (mp_limb_t)a3;
         cl = (mp_limb_t)(a3 >> 64);
     }
+#endif
     for (; i < na; i++) {
         __uint128_t a = (__uint128_t)numa[i] * x + cl;
         dst[i] = (mp_limb_t)a;
@@ -63,28 +89,49 @@ mp_limb_t lmmp_addmul_1_(mp_ptr numa, mp_srcptr numb, mp_size_t n, mp_limb_t b) 
     return cl;
 }
 
+/*
+    双借位分解（与 addmul_1 的 add/adc 结构对称，便于编译器生成短进位链）：
+
+        u = numa[i] - lo(numb[i]*b)          b1 = [u > o]   （与 d 无关，可提前）
+        r = u - d                            b2 = [r > u]
+        d' = hi(numb[i]*b) + b1 + b2
+
+    正确性：r = numa[i] - lo - d (mod B)；债务 d' = hi + ceil((lo + d - numa)/B)。
+    由于 lo + d <= 2B-2，ceil 项可取 0/1/2，恰为 b1 + b2（b1、b2 分别对应两次
+    减法各自的回绕，可同时为 1），故 d' <= B-1 仍为完整 limb 借位。
+    相比 128 位回绕减写法（sub/sbb/sub/sbb/neg 长串行链），本写法将
+    「hi 累积」与「借位判定」拆为两条独立链，GCC/Clang 均能更好调度。
+    展开 8 路：clang 下 4/8 路性能相同，gcc 下 4 路实测慢约 25%，故统一取 8 路。
+*/
 mp_limb_t lmmp_submul_1_(mp_ptr numa, mp_srcptr numb, mp_size_t n, mp_limb_t b) {
-    /* 128 位回绕减：a>>64 为 0 或全 1，取反即借位 0/1 */
-    mp_limb_t cl = 0;
+    mp_limb_t d = 0;
     mp_size_t i = 0;
 
-    for (; i + 4 <= n; i += 4) {
-        __uint128_t a0 = (__uint128_t)numa[i + 0] - (__uint128_t)numb[i + 0] * b - cl;
-        __uint128_t a1 = (__uint128_t)numa[i + 1] - (__uint128_t)numb[i + 1] * b - (mp_limb_t)(-(mp_limb_t)(a0 >> 64));
-        __uint128_t a2 = (__uint128_t)numa[i + 2] - (__uint128_t)numb[i + 2] * b - (mp_limb_t)(-(mp_limb_t)(a1 >> 64));
-        __uint128_t a3 = (__uint128_t)numa[i + 3] - (__uint128_t)numb[i + 3] * b - (mp_limb_t)(-(mp_limb_t)(a2 >> 64));
-        numa[i + 0] = (mp_limb_t)a0;
-        numa[i + 1] = (mp_limb_t)a1;
-        numa[i + 2] = (mp_limb_t)a2;
-        numa[i + 3] = (mp_limb_t)a3;
-        cl = (mp_limb_t)(-(mp_limb_t)(a3 >> 64));
+#define LMMP_SUBMUL_1_STEP(k)                           \
+    {                                                   \
+        __uint128_t P = (__uint128_t)numb[i + (k)] * b; \
+        mp_limb_t o = numa[i + (k)];                    \
+        mp_limb_t u = o - (mp_limb_t)P;                 \
+        mp_limb_t r = u - d;                            \
+        d = (mp_limb_t)(P >> 64) + (u > o) + (r > u);   \
+        numa[i + (k)] = r;                              \
+    }
+
+    for (; i + 8 <= n; i += 8) {
+        LMMP_SUBMUL_1_STEP(0)
+        LMMP_SUBMUL_1_STEP(1)
+        LMMP_SUBMUL_1_STEP(2)
+        LMMP_SUBMUL_1_STEP(3)
+        LMMP_SUBMUL_1_STEP(4)
+        LMMP_SUBMUL_1_STEP(5)
+        LMMP_SUBMUL_1_STEP(6)
+        LMMP_SUBMUL_1_STEP(7)
     }
     for (; i < n; i++) {
-        __uint128_t a = (__uint128_t)numa[i] - (__uint128_t)numb[i] * b - cl;
-        numa[i] = (mp_limb_t)a;
-        cl = (mp_limb_t)(-(mp_limb_t)(a >> 64));
+        LMMP_SUBMUL_1_STEP(0)
     }
-    return cl;
+#undef LMMP_SUBMUL_1_STEP
+    return d;
 }
 
 void lmmp_mullo_basecase_(mp_ptr restrict dst, mp_srcptr restrict numa, mp_srcptr restrict numb, mp_size_t n) {
--- src/lmmp/lmmpn/generic/shl.c
+++ src/lmmp/lmmpn/generic/shl.c
@@ -5,7 +5,7 @@
  *
  *  LMMP is free software: you can redistribute it and/or modify it under
  *  the terms of the GNU Lesser General Public License (LGPL) as published
- *   by the Free Software Foundation; either version 3 of the License, or
+ *  by the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed WITHOUT ANY WARRANTY.
@@ -16,82 +16,122 @@
 #include "../../../../include/lmmp/lmmpn.h"
 
 
-mp_limb_t lmmp_shl_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_size_t shr) {
-    if (shr == 0) {
+mp_limb_t lmmp_shl_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_size_t shl) {
+    if (shl == 0) {
         lmmp_copy(dst, numa, na);
         return 0;
-    } else {
-        const mp_limb_t rshr = LIMB_BITS - shr;
-        mp_limb_t high_limb, low_limb;
-        mp_limb_t retval;
-        numa += na;
-        dst += na;
-        low_limb = *--numa;
-        retval = low_limb >> rshr;
-        high_limb = (low_limb << shr);
-        while (--na != 0) {
-            low_limb = *--numa;
-            *--dst = high_limb | (low_limb >> rshr);
-            high_limb = (low_limb << shr);
-        }
-        *--dst = high_limb;
-        return retval;
     }
+    const mp_size_t rshr = LIMB_BITS - shl;
+    mp_limb_t retval = numa[na - 1] >> rshr;
+    if ((uintptr_t)dst < (uintptr_t)numa || (mp_size_t)(dst - numa) >= na) {
+        for (mp_size_t i = 1; i < na; i++) dst[i] = (numa[i] << shl) | (numa[i - 1] >> rshr);
+        dst[0] = numa[0] << shl;
+    } else {
+        mp_limb_t high = numa[na - 1] << shl;
+        mp_size_t i = na - 1;
+        for (; i >= 4; i -= 4) {
+            mp_limb_t l0 = numa[i - 4], l1 = numa[i - 3], l2 = numa[i - 2], l3 = numa[i - 1];
+            dst[i] = high | (l3 >> rshr);
+            dst[i - 1] = (l3 << shl) | (l2 >> rshr);
+            dst[i - 2] = (l2 << shl) | (l1 >> rshr);
+            dst[i - 3] = (l1 << shl) | (l0 >> rshr);
+            high = l0 << shl;
+        }
+        for (; i >= 1; i--) {
+            mp_limb_t l = numa[i - 1];
+            dst[i] = high | (l >> rshr);
+            high = l << shl;
+        }
+        dst[0] = high;
+    }
+    return retval;
 }
 
-mp_limb_t lmmp_shl_c_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_size_t shr, mp_limb_t c) {
-    if (shr == 0) {
+mp_limb_t lmmp_shl_c_(mp_ptr dst, mp_srcptr numa, mp_size_t na, mp_size_t shl, mp_limb_t c) {
+    if (shl == 0) {
         lmmp_copy(dst, numa, na);
         return 0;
-    } else {
-        const mp_limb_t rshr = LIMB_BITS - shr;
-        mp_limb_t high_limb, low_limb;
-        mp_limb_t retval;
-        numa += na;
-        dst += na;
-        low_limb = *--numa;
-        retval = low_limb >> rshr;
-        high_limb = (low_limb << shr);
-        while (--na != 0) {
-            low_limb = *--numa;
-            *--dst = high_limb | (low_limb >> rshr);
-            high_limb = (low_limb << shr);
-        }
-        c &= ((mp_limb_t)1 << shr) - 1;
-        *--dst = high_limb | c;
-        return retval;
     }
+    const mp_size_t rshr = LIMB_BITS - shl;
+    mp_limb_t retval = numa[na - 1] >> rshr;
+    if ((uintptr_t)dst < (uintptr_t)numa || (mp_size_t)(dst - numa) >= na) {
+        for (mp_size_t i = 1; i < na; i++) dst[i] = (numa[i] << shl) | (numa[i - 1] >> rshr);
+        c &= ((mp_limb_t)1 << shl) - 1;
+        dst[0] = (numa[0] << shl) | c;
+    } else {
+        mp_limb_t high = numa[na - 1] << shl;
+        mp_size_t i = na - 1;
+        for (; i >= 4; i -= 4) {
+            mp_limb_t l0 = numa[i - 4], l1 = numa[i - 3], l2 = numa[i - 2], l3 = numa[i - 1];
+            dst[i] = high | (l3 >> rshr);
+            dst[i - 1] = (l3 << shl) | (l2 >> rshr);
+            dst[i - 2] = (l2 << shl) | (l1 >> rshr);
+            dst[i - 3] = (l1 << shl) | (l0 >> rshr);
+            high = l0 << shl;
+        }
+        for (; i >= 1; i--) {
+            mp_limb_t l = numa[i - 1];
+            dst[i] = high | (l >> rshr);
+            high = l << shl;
+        }
+        c &= ((mp_limb_t)1 << shl) - 1;
+        dst[0] = high | c;
+    }
+    return retval;
 }
 
 mp_limb_t lmmp_addshl1_n_(mp_ptr dst, mp_srcptr numa, mp_srcptr numb, mp_size_t n) {
-    mp_size_t i, c = 0, mb = 0;
+    mp_limb_t c = 0, mb = 0;
+    mp_size_t i = 0;
 
-    for (i = 0; i < n; i++) {
-        mp_limb_t a, b, r;
-        a = numa[i];
-        b = (numb[i] << 1) + mb;
-        mb = numb[i] >> (LIMB_BITS - 1);
-        r = a + c;
-        c = (r < c);
-        r += b;
-        c += (r < b);
-        dst[i] = r;
+#define LMMP_ADDSHL1_STEP(k)                                    \
+    {                                                           \
+        mp_limb_t v = numb[i + (k)];                            \
+        mp_limb_t lo = (v << 1) | mb;                           \
+        mb = v >> (LIMB_BITS - 1);                              \
+        __uint128_t s = (__uint128_t)lo + numa[i + (k)] + c;    \
+        dst[i + (k)] = (mp_limb_t)s;                            \
+        c = (mp_limb_t)(s >> 64);                               \
     }
+
+    for (; i + 4 <= n; i += 4) {
+        LMMP_ADDSHL1_STEP(0)
+        LMMP_ADDSHL1_STEP(1)
+        LMMP_ADDSHL1_STEP(2)
+        LMMP_ADDSHL1_STEP(3)
+    }
+    for (; i < n; i++) {
+        LMMP_ADDSHL1_STEP(0)
+    }
+#undef LMMP_ADDSHL1_STEP
     return c + mb;
 }
 
 mp_limb_t lmmp_subshl1_n_(mp_ptr dst, mp_srcptr numa, mp_srcptr numb, mp_size_t n) {
-    mp_size_t i, c = 0, mb = 0;
+    mp_limb_t d = 0, mb = 0;
+    mp_size_t i = 0;
 
-    for (i = 0; i < n; i++) {
-        mp_limb_t a, b;
-        a = numa[i];
-        b = (numb[i] << 1) + mb;
-        mb = numb[i] >> (LIMB_BITS - 1);
-        b += c;
-        c = (b < c);
-        c += (a < b);
-        dst[i] = a - b;
+#define LMMP_SUBSHL1_STEP(k)                             \
+    {                                                    \
+        mp_limb_t v = numb[i + (k)];                     \
+        mp_limb_t lo = (v << 1) | mb;                    \
+        mb = v >> (LIMB_BITS - 1);                       \
+        mp_limb_t o = numa[i + (k)];                     \
+        mp_limb_t u = o - lo;                            \
+        mp_limb_t r = u - d;                             \
+        d = (u > o) + (r > u);                           \
+        dst[i + (k)] = r;                                \
     }
-    return c + mb;
+
+    for (; i + 4 <= n; i += 4) {
+        LMMP_SUBSHL1_STEP(0)
+        LMMP_SUBSHL1_STEP(1)
+        LMMP_SUBSHL1_STEP(2)
+        LMMP_SUBSHL1_STEP(3)
+    }
+    for (; i < n; i++) {
+        LMMP_SUBSHL1_STEP(0)
+    }
+#undef LMMP_SUBSHL1_STEP
+    return d + mb;
 }
